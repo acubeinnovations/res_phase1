402f3c896ef9b7336b8b5758561bd8674818274d
Acube Innovation <pramodgmenon@acube.co>
2013-12-20 12:53:27 +0530

    counter menu modified

--- include/counter/inc_right_menu.php
+++ include/counter/inc_right_menu.php
@@ -4,12 +4,15 @@
 <?php if(isset($_SESSION[SESSION_TITLE.'counter_userid']) && $_SESSION[SESSION_TITLE.'counter_userid'] > 0){ ?>
         <li class="divider"></li>
         <li>
+		<?php if ($this->page_name == 'counter_dashboard'){ ?>
          <a href="#" data-reveal-id="openedbills" id="opened_bills">Bill on Hold</a>
-          
+        <?php }else{ ?>
+		<a href="dashboard.php" >Go To Counter</a>
+        <?php } ?>
         </li>
 		<li>
          <a href="bills.php">Bills</a>
-          
+
         </li>
 
 
