5af097ddc7e3a121c36d318c175b99e9cf288f1f
Arun <arunr@acube.co>
2013-12-26 15:55:34 +0530

    modified script on holdbill

--- modules/bill/update/script.php
+++ modules/bill/update/script.php
@@ -439,8 +439,9 @@ $(document).keypress(function(e) {
 		if(data=='1'){
 			$(".bill").html('&nbsp');
 			$('#tot_button_val').text("TOTAL : Rs .0");
-			alert("bill holded");
-			location.reload();
+			popup_alert("Bill holded","dashboard.php","Ok","false");
+			//location.reload();
+			
 		}else{
 			popup_alert("Bill cannot be holded","");
 		}
