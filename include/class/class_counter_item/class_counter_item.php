f4a35f2ae1bad25ac7c4e25d87fa0e5dc83fd1b7
Arun <arunr@acube.co>
2013-12-27 13:23:47 +0530

    add items stock completed

--- include/class/class_counter_item/class_counter_item.php
+++ include/class/class_counter_item/class_counter_item.php
@@ -45,14 +45,14 @@ function get_item_quantity_today()
 	$available_quantity=0;
 	
 	$strSQL = "SELECT sum(quantity) as total_quantity FROM counter_items WHERE  DATE_FORMAT(date,'%Y-%m-%d') = '".date("Y-m-d")."' AND counter_id = '".$this->counter_id."'"." AND item_id = '".$this->item_id."'";
-		echo $strSQL1 = "SELECT sum(quantity) as bill_item_total_quantity FROM bill_items WHERE  created= '".date("Y-m-d")."' AND counter_id = '".$this->counter_id."'"." AND item_id = '".$this->item_id. "' AND bill_item_status_id='".BILL_ITEM_STATUS_ACTIVE."'";
+		 $strSQL1 = "SELECT sum(quantity) as bill_item_total_quantity FROM bill_items WHERE  created LIKE '".date("Y-m-d")."%' AND counter_id = '".$this->counter_id."'"." AND item_id = '".$this->item_id. "' AND bill_item_status_id='".BILL_ITEM_STATUS_ACTIVE."'";
 	//echo $strSQL;
 	$rsRES	= mysql_query($strSQL,$this->connection) or die(mysql_error().$strSQL);
 	$rsRES1	= mysql_query($strSQL1,$this->connection) or die(mysql_error().$strSQL1);
 	 if(mysql_num_rows($rsRES) > 0){
 		
-			echo $this->quantity =mysql_result($rsRES,0,'total_quantity');
-			echo $this->bill_item_total_quantity =mysql_result($rsRES1,0,'bill_item_total_quantity');
+			$this->quantity =mysql_result($rsRES,0,'total_quantity');
+			$this->bill_item_total_quantity =mysql_result($rsRES1,0,'bill_item_total_quantity');
 			$available_quantity=0;
 			$available_quantity=($this->quantity)-($this->bill_item_total_quantity);
 			return $available_quantity;
