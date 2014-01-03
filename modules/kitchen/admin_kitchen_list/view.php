956f2b8e36aacf9020efe9c9427048ac44e7df5c
febeena <febeena@acube.co>
2014-01-03 18:07:22 +0530

    kitchen list_report modified

--- modules/kitchen/admin_kitchen_list/view.php
+++ modules/kitchen/admin_kitchen_list/view.php
@@ -43,8 +43,8 @@
       <td><?php echo $array_kitchen[$i]['username']?></td>
          <td colspan="2"> <?php echo $array_kitchen[$i]['name'] ;?></td>
           <td> <?php echo $array_kitchen[$i]['lastlogin'] ;?></td>
-           <td colspan="2">  <?php if(isset($g_ARRAY_STATUS[$array_kitchen[$i]['status_id']])){ echo $g_ARRAY_STATUS[$array_kitchen[$i]['status_id']];}?></td>
-            <td colspan="4"> <a href="admin_kitchen_sales_report.php?id=<?php echo $array_kitchen[$i]['id']?>">View Report</a></td>
+           <td colspan="2"> <?php if(isset($g_ARRAY_STATUS[$array_kitchen[$i]['status_id']])){ echo $g_ARRAY_STATUS[$array_kitchen[$i]['status_id']];}?></td>
+            <td colspan="4"><a href="admin_kitchen_sales_report.php?kitchen_id=<?php echo $array_kitchen[$i]['id']?>&counter_id=<?php echo $array_kitchen[$i]['counter_id']?>">View Report</a></td>
 
       </td>
       </tr>
