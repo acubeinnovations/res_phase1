23467b9eced7256241b0bf874a08729dc5b4a4e1
Nijo Joseph <nijojoseph@acube.co>
2014-01-17 15:18:48 +0530

    view page

--- modules/back_up/back_up_page/view.php
+++ modules/back_up/back_up_page/view.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="medium-12 columns counter">
 		<div class="medium-8 columns ">
-			<div class="medium-12 columns counter"><div class="medium-3 columns counter"><a href="#" class="backup button success">SYNC-DATABASE</a></div><div class="medium-9 columns counter"><div id="message" style="display:none;"><br><font color="green">Synchronizing data.Please dont close the window....!!</font></div></div></div>
+			<div class="medium-12 columns counter"><div class="medium-4 columns counter"><a href="#" class="backup button success">SYNC-DATABASE</a></div><div class="medium-7 columns counter"><div id="message" style="display:none;"><br><font color="green">Synchronizing data.Please dont close the window....!!</font></div></div></div>
 		</div>
 		<div class="medium-2 columns ">
 		</div>
